f2398151414f1fbcc8cb00f76aa579929186d505
Gilgamesh1719 <ltnkmk@gmail.com>
2025-06-26 11:31:15 +0600

    Added Vue counter

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>Счетчик: {{ count }}</h1>
+    <button @click="increment">Увеличить</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+};
+</script>
+
+<style>
+/* Можно оставить пустым или добавить стили по желанию */
+</style>
